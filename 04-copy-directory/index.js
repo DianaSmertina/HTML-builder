8b4c218d903648787d90c1b505b08073b2497376
DianaSmertina <am.dianasmertina@gmail.com>
2022-11-06 19:22:27 +0400

    feat: add desision for 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+
+function addFiles () {
+  fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, dirEntryList) => {
+    if (err) throw err;
+    dirEntryList.forEach(el => {
+      fs.copyFile(path.join(__dirname, 'files', el.name), path.join(__dirname, 'files-copy', el.name), err => {
+        if (err) throw err;
+      })
+    });
+  })
+} 
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+  fs.readdir(path.join(__dirname, 'files-copy'), {withFileTypes: true}, (err, dirEntryListCopy) => {
+    if (err) throw err;
+    if (dirEntryListCopy !== []) {
+      dirEntryListCopy.forEach(el => {
+        fs.unlink(path.join(__dirname, 'files-copy', el.name), err => {
+          if (err) throw err;
+        })
+      });
+    } 
+      addFiles();
+  })
+})
